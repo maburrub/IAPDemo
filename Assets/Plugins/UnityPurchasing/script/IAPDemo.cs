051f9602726543d59a198b9006276b5ff4bc27e2
Mohammed Abualrob <mohammed@unity3d.com>
2016-07-26 15:25:32 -0700

    selecting google play at run time

--- Assets/Plugins/UnityPurchasing/script/IAPDemo.cs
+++ Assets/Plugins/UnityPurchasing/script/IAPDemo.cs
@@ -173,7 +173,7 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 
 	public void Awake()
 	{
-		var module = StandardPurchasingModule.Instance();
+		var module = StandardPurchasingModule.Instance(AndroidStore.GooglePlay);
 
 		// The FakeStore supports: no-ui (always succeeding), basic ui (purchase pass/fail), and 
 		// developer ui (initialization, purchase, failure code setting). These correspond to 
