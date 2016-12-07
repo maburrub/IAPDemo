7b5687bef5b92796f8e74b62730a2efa10e1133d
Mohammed Abualrob <mohammed@unity3d.com>
2016-12-07 14:11:37 -0800

    tvOS initial setup

--- Assets/Plugins/UnityPurchasing/script/IAPDemo.cs
+++ Assets/Plugins/UnityPurchasing/script/IAPDemo.cs
@@ -258,7 +258,7 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 		// and we tell Unity IAP this by using the IDs class.
 		builder.AddProduct("coins", ProductType.Consumable, new IDs
 		{
-				{"com.unity3d.unityiap.unityiapdemo.100goldcoins.6", AppleAppStore.Name},
+				{"100.gold.coins.1", AppleAppStore.Name},
 				{"com.unity3d.unityiap.unityiapdemo.100goldcoins.7", MacAppStore.Name},
 				{"com.eight.bit.avenue.amorcam.100coins.3", GooglePlay.Name},
 				{"com.eight.bit.avenue.100coins.1", WindowsStore.Name},
@@ -270,7 +270,7 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 
 		builder.AddProduct("sword", ProductType.NonConsumable, new IDs
 		{
-				{"com.unity3d.unityiap.unityiapdemo.sword.6", AppleAppStore.Name},
+				{"sword.1", AppleAppStore.Name},
 				{"com.unity3d.unityiap.unityiapdemo.sword.7", MacAppStore.Name},
 				{"com.eight.bit.avenue.amorcam.sword.3", GooglePlay.Name},
 				{"com.eight.bit.avenue.sword.1", WindowsStore.Name},
@@ -281,7 +281,7 @@ public class IAPDemo : MonoBehaviour, IStoreListener
 
 		builder.AddProduct("subscription", ProductType.Subscription, new IDs
 		{
-				{"com.unity3d.unityiap.unityiapdemo.subscription", AppleAppStore.Name},
+				{"subscription.1", AppleAppStore.Name},
 				{"com.unity3d.unityiap.unityiapdemo.subscription.7", MacAppStore.Name},
 				{"com.eight.bit.avenue.amorcam.subscription.3", GooglePlay.Name},
 				{"com.eight.bit.avenue.subscription.1", WindowsStore.Name},
