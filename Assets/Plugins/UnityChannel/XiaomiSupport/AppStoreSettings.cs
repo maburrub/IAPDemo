20ef080909523d68e51a8082e45aeb4e35959aed
Mohammed Abualrob <mohammed@unity3d.com>
2017-07-20 14:26:16 -0700

    IAP 1.12 rc4 + Unity 2017.1.0f3

---
+++ Assets/Plugins/UnityChannel/XiaomiSupport/AppStoreSettings.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_5_6_OR_NEWER && !UNITY_5_6_0
+using UnityEngine;
+using UnityEngine.Store;
+
+namespace AppStoresSupport
+{
+    [System.Serializable]
+    public class AppStoreSetting 
+    {
+        public string AppID = "";
+        public string AppKey = "";
+        public bool IsTestMode = false;
+    }
+
+    [CreateAssetMenu(fileName = "Assets/Plugins/UnityChannel/XiaomiSupport/Resources/AppStoreSettings.asset", menuName = "App Store Settings")]
+    [System.Serializable]
+    public class AppStoreSettings : ScriptableObject
+    {
+        public string UnityClientID = "";
+        public string UnityClientKey = "";
+        public string UnityClientRSAPublicKey = "";
+
+        public AppStoreSetting XiaomiAppStoreSetting = new AppStoreSetting();
+		
+		public AppInfo getAppInfo() {
+			AppInfo appInfo = new AppInfo();
+			appInfo.clientId = UnityClientID;
+			appInfo.clientKey = UnityClientKey;
+			appInfo.appId = XiaomiAppStoreSetting.AppID;
+			appInfo.appKey = XiaomiAppStoreSetting.AppKey;
+			appInfo.debug = XiaomiAppStoreSetting.IsTestMode;
+			return appInfo;
+		}
+    }
+}
+#endif
